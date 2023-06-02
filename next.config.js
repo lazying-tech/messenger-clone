c317a0118b5aaf92a5c9b2ee24ca7aff0d6ccfe4
lazying-tech <boylmhtsingle@gmail.com>
2023-06-02 19:31:11 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        destination: "http://localhost:8000//api/v1/:path*",
+        destination: "http://localhost:8000/api/v1/:path*",
       },
     ];
   },
